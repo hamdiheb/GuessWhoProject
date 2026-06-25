6126ef99deec11bea9a21edc06eb57a36d48d850
Alex <oleksi.ovsiannikov@gmail.com>
2026-06-26 00:03:36 +0200

    Create Sign Up page layout

--- GuessWho/frontend/signup/SignUp.jsx
+++ GuessWho/frontend/signup/SignUp.jsx
@@ -1,8 +1,51 @@
+import './SignUp.css'
+
+import email_icon from './Assets_SignUp/email.jpg'
+
+
 function SignUp() {
   return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
+        <div className='container'>
+            <div className='header'>
+                <div className="text">Sign Up</div>
+                <div className='underline'></div>
+            </div>
+            <div className='inputs'>
+
+                 <div className='input'>
+                    <input 
+                    type='text'
+                    placeholder='Username'
+                    />
+                </div>
+
+                <div className='input'>
+                    <img src={email_icon} alt='Email' />
+                    <input 
+                    type='email'
+                    placeholder='Email'
+                    />
+                </div>
+
+                
+               
+                <div className='input'>
+                    <input 
+                    type='password'
+                    placeholder='Password'
+                    />
+                </div>  
+
+            </div> 
+
+            <button className='submit'>
+                Sign Up
+            </button>
+
+            <div className='login-link'>
+                Already have an account? <span>Sign In</span>
+            </div>
+        </div>
   );
 }
 
