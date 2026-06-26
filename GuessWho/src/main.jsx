5bebd0602c6f5ba3e2fa277f0bf3ab1d71992353
sahimsaami <sahimsaam@gmail.com>
2026-06-26 22:51:57 +0200

    fixed Dashboard name

--- GuessWho/src/main.jsx
+++ GuessWho/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import Dashboard from '../frontend/dashboard/dashboard.jsx'
+import Dashboard from '../frontend/dashboard/Dashboard.jsx';  
 
 createRoot(document.getElementById('root')).render(
   <Dashboard /> 
