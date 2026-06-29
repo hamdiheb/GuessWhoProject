21f6fe380f56b409f15d47c07b8a81b3539cbec8
Alex <oleksi.ovsiannikov@gmail.com>
2026-06-30 00:06:51 +0200

    Move routing to App component

--- GuessWho/src/main.jsx
+++ GuessWho/src/main.jsx
@@ -4,16 +4,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import './index.css'
-import SignUp from '../frontend/signup/SignUp.jsx'
-import Dashboard from '../frontend/dashboard/Dashboard.jsx';  
-
+import App from './App'
 createRoot(document.getElementById('root')).render(
      <StrictMode>
       <BrowserRouter>
-      <Routes>
-      <Route path='/' element={<SignUp/>}/>
-      <Route path='/dashboard' element={<BrowserRouter/>} />
-      </Routes>
+      <App />
       </BrowserRouter>
   </StrictMode>
 );
