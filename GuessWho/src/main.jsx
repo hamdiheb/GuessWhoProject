a97ef4f6419b0cd243af625c8d21551831d7c097
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2026-06-30 14:25:39 +0200

    Setup database with game_history and users table

--- GuessWho/src/main.jsx
+++ GuessWho/src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react';
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './index.css'
 import App from './App'
 createRoot(document.getElementById('root')).render(
-     <StrictMode>
-      <BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
       <App />
-      </BrowserRouter>
-  </StrictMode>
-);
+    </BrowserRouter>
+  </StrictMode>,
+)
