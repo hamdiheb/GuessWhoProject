62877f84542271abc24037b0130bd419fc3fefd7
Priscilla Mac-Gatus <176624280+Priscilla-MacGatus@users.noreply.github.com>
2026-07-08 22:09:33 +0200

    signin with appjsx

---
+++ GuessWho/frontend/src/signin/signinApp.jsx
@@ -0,0 +1,18 @@
+lsimport { Routes, Route } from "react-router-dom";
+
+import SignIn from "./signin/SignIn";
+import SignUp from "./signup/SignUp";
+import Dashboard from "./dashboard/Dashboard";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<SignUp />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
+
+export default App;
