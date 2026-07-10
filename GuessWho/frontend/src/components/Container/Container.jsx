d0d88a327ed9bad2d025c1ffa06eedf685155871
Alex <oleksi.ovsiannikov@gmail.com>
2026-07-10 19:59:06 +0200

    refactor: allow custom className for Container

--- GuessWho/frontend/src/components/Container/Container.jsx
+++ GuessWho/frontend/src/components/Container/Container.jsx
@@ -1,8 +1,8 @@
 import styles from "./Container.module.css";
 
-export default function Container({ children }) {
+export default function Container({ children, className = '' }) {
   return (
-    <div className={`${styles.container}`}>
+    <div className={`${styles.container} ${className}`}>
       {children}
     </div>
   );
