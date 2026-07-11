54955977c757bee7ddfb4b60b5b964c979d3c783
Alex <oleksi.ovsiannikov@gmail.com>
2026-07-11 14:42:22 +0200

    Implement Sign In page

---
+++ GuessWho/frontend/src/signin/SignIn.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./SignIn.module.css";
+import { supabase } from "../../../backend/server";
+
+
+
+function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  setErrorMessage("");
+  setSuccessMessage("");
+
+  if (!email.trim() || !password.trim()) {
+    setErrorMessage("All fields are required.");
+    return;
+  }
+
+   try {
+    const response = await fetch("http://localhost:3000/api/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      setErrorMessage(data.message);
+      return;
+    }
+    localStorage.setItem("currentUserId", data.id);
+
+    setSuccessMessage("Welcome back!");
+
+    setTimeout(() => {
+      navigate("/dashboard");
+    }, 1000);
+  } catch (err) {
+    console.error(err);
+    setErrorMessage("Cannot connect to the server.");
+  }
+};
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.text}>Sign In</div>
+        <div className={styles.underline}></div>
+      </div>
+
+    <form onSubmit={handleSubmit}>
+  <div className={styles.inputs}>
+    <div className={styles.input}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+    </div>
+
+    <div className={styles.input}>
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+    </div>
+  </div>
+
+  {successMessage && (
+    <div className={styles.successMessage}>
+      {successMessage}
+    </div>
+  )}
+
+  {errorMessage && (
+    <div className={styles.errorMessage}>
+      {errorMessage}
+    </div>
+  )}
+
+  <button className={styles.submit} type="submit">
+    Sign In
+  </button>
+</form>
+
+      <div className={styles.signupLink}>
+        Don't have an account?{" "}
+        <span onClick={() => navigate("/")}>
+          Sign Up
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
